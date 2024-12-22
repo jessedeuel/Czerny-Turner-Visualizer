7c5f476ce340350275cb5dc642bf778bfb68d6e9
Ironside32 <jesse@deuels.net>
2024-12-21 21:23:08 -0800

    Switched to raylib :(

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,6 @@
-#include <Magnum/GL/DefaultFramebuffer.h>
-#include <Magnum/Platform/Sdl2Application.h>
+#include "raylib.h"
 
-using namespace Magnum;
+int main() {
 
-class MyApplication: public Platform::Application {
-    public:
-        explicit MyApplication(const Arguments& arguments);
-
-    private:
-        void drawEvent() override;
-};
-
-MyApplication::MyApplication(const Arguments& arguments): Platform::Application{arguments} {
-    /* TODO: Add your initialization code here */
+    return 0;
 }
-
-void MyApplication::drawEvent() {
-    GL::defaultFramebuffer.clear(GL::FramebufferClear::Color);
-
-    /* TODO: Add your drawing code here */
-
-    swapBuffers();
-}
-
-MAGNUM_APPLICATION_MAIN(MyApplication)
